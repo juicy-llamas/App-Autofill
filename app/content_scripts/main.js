ba1c2ddd662d6e4254d74b301e062cc23d24a4d1
Daniel Moylan <demoylan@gmail.com>
2022-09-16 17:41:42 -0400

    starting to implement blocker

--- app/content_scripts/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// by the time this script has finished loading, the document would be loaded, so we can just run our code as normal.
-{
-	console.log( "hello world from console (extension)" );
-}
